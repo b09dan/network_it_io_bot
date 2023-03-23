77cc73ba9912fd74d397c4caa0e379a9b2432d58
d skorinov <d.skorinov@gmail.com>
2023-03-23 15:21:52 +0100

    EST-001 Parameters evaluation

--- competition/estimation.py
+++ competition/estimation.py
@@ -1,30 +1,54 @@
 import csv
-sort_list = ["nickname", "place", "ept points", "average", "offense", "defense", "versatility", "multitasking", "mechanics", "speed"]
-final_list = []
+import numpy as np
+from scipy.stats import pearsonr, spearmanr
 
-with open('players.csv', 'r') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=';')
-    next(csvreader)
-    sorted_data = sorted(csvreader, key=lambda row: float(row[9]))
+def parse_csv(file_name):
+    result = {}
+    with open(file_name, 'r', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        headers = next(reader)
+        for index, header in enumerate(headers):
+            result[header] = []
 
-summs_list = []
+        #dict with list of lists
+        for row_num, row in enumerate(reader, start=1):
+            for col_num, value in enumerate(row):
+                result[headers[col_num]].append((row_num, float(value)))
+        #sort all by value
+        for header in headers:
+            if "place" in header:
+                result[header].sort(key=lambda x: x[1])
+            else:
+                result[header].sort(key=lambda x: x[1], reverse=True)
 
-for i in range(1,10):
-    summ = 0
-    ind = 1
-    print("The summ for:", sort_list[i])
-    sorted_data.sort(key=lambda x: float(x[i]), reverse=True)
-    for row in sorted_data:
-        summ += (float(row[1])-float(ind))**2
-        print(row[0] + " "  + str(row[1]) + " "  + str(ind) + " " + str(row[i]))
-        ind += 1
-    summs_list.append(summ)
+    return result
 
-for i in range(9):
-    final_list.append(str(summs_list[i]) +  " --- " +  str(sort_list[i+1]))
+def calculate(parsed_csv):
+    list_result = []
+    for key in parsed_csv:
+        if "nickname" not in key:
+            places_array = np.array([float(item[0]) for item in parsed_csv[key]])
+            values_array = np.array([float(item[1]) for item in parsed_csv[key]])
+            rangs_array = np.array([index + 1 for index, _ in enumerate(parsed_csv[key])])
 
+            pearson_corr, pearson_p = pearsonr(places_array, values_array)
+            spearman_corr, spearman_p = spearmanr(places_array, rangs_array)
 
-final_list.sort()
+            list_result.append([key, round(pearson_corr,2), round(pearson_p,2), round(spearman_corr, 2), round(spearman_p, 2)])
+            #print(places_array)
+            #print(values_array)
+            #print(rangs_array)
 
-for l in final_list:
-    print(l)
+    return list_result
+
+## main
+list_result = []
+file_name = 'data/players.csv'
+
+parsed_csv = parse_csv(file_name)
+list_result = calculate(parsed_csv)
+
+list_result.sort(key=lambda x: x[3], reverse=True)        
+for elem in list_result:
+    print(elem)
+
